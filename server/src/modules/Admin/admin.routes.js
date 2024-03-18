02790809c36a05ec8eebd2506cc1822f469e7010
naineel1209 <soyantarnaineel@gmail.com>
2024-03-18 22:35:43 +0530

    added update methods in the admin module

--- server/src/modules/Admin/admin.routes.js
+++ server/src/modules/Admin/admin.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { validate } from "express-validation";
 import passport from "passport";
-import { loginAdmin, registerAdmin } from "./admin.controllers.js";
-import { adminLoginSchema, adminRegisterSchema } from "./admin.schema.js";
+import { loginAdmin, registerAdmin, updateAdmin } from "./admin.controllers.js";
+import { adminLoginSchema, adminRegisterSchema, adminUpdateSchema } from "./admin.schema.js";
+import { checkPermissions, isAuthenticated } from "../../middlewares/middlewares.js";
+import Admin from "./Admin.js";
 const router = Router({ mergeParams: true })
 
 //!PATH: /api/admin
@@ -13,9 +15,11 @@ router
     }), registerAdmin);
 
 router
-    .post("/login", validate(adminLoginSchema, {
-        keyByField: true,
-    }), passport.authenticate("admin", { failureRedirect: "/api/error" }), loginAdmin);
+    .post("/login", validate(adminLoginSchema, { keyByField: true, }), passport.authenticate("admin", { failureRedirect: "/api/error" }), loginAdmin);
+
+router
+    .patch("/update/:adminId", isAuthenticated, checkPermissions(Admin), validate(adminUpdateSchema), updateAdmin)
+    .delete("/delete/:adminId", isAuthenticated, checkPermissions(Admin), validate(adminDeleteSchema),);
 
 
 export default router;
