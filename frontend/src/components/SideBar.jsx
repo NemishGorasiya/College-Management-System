90f3bf875ac03f7e3af2f5bc1b48df39c2c99a35
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-04-04 01:09:20 +0530

    Added Sidebar Animation for small devices

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,38 +1,43 @@
-import "./SideBar.scss"
+import "./SideBar.scss";
 import dashBoardIcon from "../assets/my_result.svg";
 import { NavLink } from "react-router-dom";
 
-const sideBarNavLinks = ["profile","result","placement","events","circulars","exam"]
+import { sideBarNavLinks } from "../constant/constatnt.jsx";
+import { sideBarExtraNavLinks } from "../constant/constatnt.jsx";
 
-export default function SideBar() {
-  return (
-    <div className='sideBar'>
-      <ul className='sideBarLinksWrapper'>
-        <div className="sidebarUpperLinks">
-          {
-            sideBarNavLinks.map((link)=><NavLink key={link} to={link} className={({ isActive }) =>
-            isActive ? "sideBarNavLink activeLink" : "sideBarNavLink"
-          }>
-            <img className='sideBarIcon' src={dashBoardIcon} alt="" />
-            <span className="sideBarLinkName">{link}</span>
-        </NavLink>)
-          }
-        </div>
-        <div className="sidebarLowerLinks">
-        <NavLink to="help" className={({ isActive }) =>
-            isActive ? "sideBarNavLink activeLink" : "sideBarNavLink"
-          }>
-            <img className='sideBarIcon' src={dashBoardIcon} alt="" />
-            <span className="sideBarLinkName">Help</span>
-        </NavLink>
-        <NavLink to="setting"  className={({ isActive }) =>
-            isActive ? "sideBarNavLink activeLink" : "sideBarNavLink"
-          }>
-            <img className='sideBarIcon' src={dashBoardIcon} alt="" />
-            <span className="sideBarLinkName">Setting</span>
-        </NavLink>
-        </div>
-      </ul>
-    </div>
-  )
+export default function SideBar({ isSideBarOpen }) {
+	return (
+		<div className={`sideBar ${isSideBarOpen ? "sideBarOpen" : ""}`}>
+			<ul className="sideBarLinksWrapper">
+				<div className="sidebarUpperLinks">
+					{sideBarNavLinks.map(({ link, label, svgIcon }) => (
+						<NavLink
+							key={link}
+							to={link}
+							className={({ isActive }) =>
+								isActive ? "sideBarNavLink activeLink" : "sideBarNavLink"
+							}
+						>
+							<img className="sideBarIcon" src={svgIcon} alt="" />
+							<span className="sideBarLinkName">{label}</span>
+						</NavLink>
+					))}
+				</div>
+				<div className="sidebarLowerLinks">
+					{sideBarExtraNavLinks.map(({ link, label, svgIcon }) => (
+						<NavLink
+							key={link}
+							to={link}
+							className={({ isActive }) =>
+								isActive ? "sideBarNavLink activeLink" : "sideBarNavLink"
+							}
+						>
+							<img className="sideBarIcon" src={svgIcon} alt="" />
+							<span className="sideBarLinkName">{label}</span>
+						</NavLink>
+					))}
+				</div>
+			</ul>
+		</div>
+	);
 }
