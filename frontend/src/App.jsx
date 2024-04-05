8406354dd7e2766c4c34484529384a5a14b58efd
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-04-05 16:17:01 +0530

    refactor topbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,64 +1,62 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import './App.css'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
 import Profile from "./components/Profile.jsx";
 import Result from "./components/Result.jsx";
 import Placement from "./components/Placement.jsx";
 import Events from "./components/Events.jsx";
 import Circulars from "./components/Circulars.jsx";
 import Exam from "./components/Exam.jsx";
-import HomePage from './pages/HomePage.jsx';
-import Help from './components/Help.jsx';
-import Setting from './components/Setting.jsx';
+import HomePage from "./pages/HomePage.jsx";
+import Help from "./components/Help.jsx";
+import Setting from "./components/Setting.jsx";
 
 function App() {
-
   const router = createBrowserRouter([
     {
-      path : "/",
-      element : <HomePage />,
-      children : [
+      path: "/",
+      element: <HomePage />,
+      children: [
         {
-          path : "/profile",
-          element : <Profile />
+          path: "/profile",
+          element: <Profile />,
         },
         {
-          path : "/result",
-          element : <Result />
+          path: "/result",
+          element: <Result />,
         },
         {
-          path : "/placement",
-          element : <Placement />
+          path: "/placement",
+          element: <Placement />,
         },
         {
-          path : "/events",
-          element : <Events />
+          path: "/events",
+          element: <Events />,
         },
         {
-          path : "/circulars",
-          element : <Circulars />
+          path: "/circulars",
+          element: <Circulars />,
         },
         {
-          path : "/exam",
-          element : <Exam />
+          path: "/exam",
+          element: <Exam />,
         },
         {
-          path : "/help",
-          element : <Help />
+          path: "/help",
+          element: <Help />,
         },
         {
-          path : "/setting",
-          element : <Setting />
+          path: "/setting",
+          element: <Setting />,
         },
-      ]
+      ],
     },
-    
-  ])
+  ]);
 
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
