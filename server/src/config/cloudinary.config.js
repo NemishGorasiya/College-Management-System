ce33fe7b9cf6bb52a1ddbb8581f1f29228f685d4
Naineel Soyantar <soyantarnaineel@gmail.com>
2024-04-09 15:03:53 +0530

    resolved error

--- server/src/config/cloudinary.config.js
+++ server/src/config/cloudinary.config.js
@@ -2,6 +2,10 @@ import { v2 as cloudinary } from 'cloudinary';
 import { config } from "dotenv";
 config();
 
+if (!process.env.CLOUD_NAME || !process.env.CLOUD_API_KEY || !process.env.CLOUD_API_SECRET) {
+    throw new Error("Cloudinary config is not set");
+}
+
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
