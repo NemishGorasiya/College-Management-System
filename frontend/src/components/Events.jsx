019fcdd371c7278b7a99fcdc5b32530946aa6c0c
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-04-18 00:59:51 +0530

    Added delete event functionality

--- frontend/src/components/Events.jsx
+++ frontend/src/components/Events.jsx
@@ -8,38 +8,42 @@ import UploadCircularButton from "./UploadCircularButton";
 import AddNewEventButton from "./events/AddNewEventButton";
 
 export default function Events() {
-  const [events, setEvents] = useState({
-    list: [],
-    isLoading: true,
-  });
-  const { list: eventsList, isLoading: isEventsLoading } = events;
-  const getEvents = async () => {
-    const res = await fetchEvents();
-    console.log(res);
-    setEvents({
-      list: res.events,
-      isLoading: false,
-    });
-  };
-  useEffect(() => {
-    getEvents();
-  }, []);
-  return (
-    <div className="eventsService">
-      <ServiceTitle serviceTitle="Events" />
-      <div className="eventsWrapper">
-        <AddNewEventButton getEvents={getEvents} />
-        <YearMonthFilter />
-        <div className="eventsContainer">
-          {isEventsLoading ? (
-            <h1>Loading...</h1>
-          ) : (
-            eventsList.map((event) => (
-              <EventCard key={event._id} eventDetails={event} />
-            ))
-          )}
-        </div>
-      </div>
-    </div>
-  );
+	const [events, setEvents] = useState({
+		list: [],
+		isLoading: true,
+	});
+	const { list: eventsList, isLoading: isEventsLoading } = events;
+	const getEvents = async () => {
+		const res = await fetchEvents();
+		console.log(res);
+		setEvents({
+			list: res.events,
+			isLoading: false,
+		});
+	};
+	useEffect(() => {
+		getEvents();
+	}, []);
+	return (
+		<div className="eventsService">
+			<ServiceTitle serviceTitle="Events" />
+			<div className="eventsWrapper">
+				<AddNewEventButton getEvents={getEvents} />
+				<YearMonthFilter />
+				<div className="eventsContainer">
+					{isEventsLoading ? (
+						<h1>Loading...</h1>
+					) : (
+						eventsList.map((event) => (
+							<EventCard
+								key={event._id}
+								eventDetails={event}
+								getEvents={getEvents}
+							/>
+						))
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
--- frontend/src/components/exam/ExamCard.jsx
+++ frontend/src/components/exam/ExamCard.jsx
@@ -6,80 +6,63 @@ import CloseIcon from "@mui/icons-material/Close";
 import { DateToTime, formatDate } from "../../utils/utilityFunctions.js";
 
 const ExamCard = ({ examDetails }) => {
-  const [open, setOpen] = useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+	const [open, setOpen] = useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 
-  const { name, examType, results, duration, subject, totalMarks, date } =
-    examDetails;
-  const { name: subjectName, credits, subjectCode, semester } = subject;
+	const { name, examType, duration, subject, totalMarks, date } = examDetails;
 
-  const handleCardClick = () => {
-    handleOpen();
-  };
-  return (
-    <>
-      <div className="examCardWrapper" onClick={handleCardClick}>
-        <div className="examDetailsWrapper">
-          <p className="examName">{name}</p>
-          <p className="subjectName">{subjectName}</p>
-          <p>Know More</p>
-        </div>
-      </div>
-      <Modal open={open} onClose={handleClose} className="examDetailsModal">
-        <Box sx={modalStyle}>
-          <IconButton
-            onClick={handleClose}
-            sx={{
-              position: "absolute",
-              right: 5,
-              top: 5,
-            }}
-          >
-            <CloseIcon />
-          </IconButton>
-          <p className="examTitle">{name}</p>
-          <p className="examDetail">
-            <span className="examDetailLabel">Subject : </span>
-            {subjectName}
-          </p>
-          <p className="examDetail">
-            <span className="examDetailLabel">Subject Code : </span>(
-            {subjectCode})
-          </p>
-          <p className="examDetail">
-            <span className="examDetailLabel">Semester : </span> {semester}
-          </p>
-          <p className="examDetail">
-            <span className="examDetailLabel">Total Marks : </span>
-            {totalMarks}
-          </p>
-          <p className="examDetail">
-            <span className="examDetailLabel">date : </span> {formatDate(date)}
-          </p>
-          <p className="examDetail">
-            <span className="examDetailLabel">Time : </span> {DateToTime(date)}
-          </p>
-          <p className="examDetail">
-            <span className="examDetailLabel">duration : </span> {duration}{" "}
-            hours
-          </p>
-          <p className="examDetail">
-            <span className="examDetailLabel">Subject Credits : </span>
-            {credits}
-          </p>
-          <p className="examDetail">
-            <span className="examDetailLabel">Exam Type : </span>
-            {examType}
-          </p>
-          <p className="examDetail">
-            <span className="examDetailLabel">Result : </span>
-            <span className="examResult">{results[0]?.marks ?? "Pending"}</span>
-          </p>
-        </Box>
-      </Modal>
-    </>
-  );
+	const handleCardClick = () => {
+		handleOpen();
+	};
+	return (
+		<>
+			<div className="examCardWrapper" onClick={handleCardClick}>
+				<div className="examDetailsWrapper">
+					<p className="examName">{name}</p>
+					<p className="subjectName">{subject}</p>
+					<p>Know More</p>
+				</div>
+			</div>
+			<Modal open={open} onClose={handleClose} className="examDetailsModal">
+				<Box sx={modalStyle}>
+					<IconButton
+						onClick={handleClose}
+						sx={{
+							position: "absolute",
+							right: 5,
+							top: 5,
+						}}
+					>
+						<CloseIcon />
+					</IconButton>
+					<p className="examTitle">{name}</p>
+					<p className="examDetail">
+						<span className="examDetailLabel">Subject : </span>
+						{subject}
+					</p>
+					<p className="examDetail">
+						<span className="examDetailLabel">Total Marks : </span>
+						{totalMarks}
+					</p>
+					<p className="examDetail">
+						<span className="examDetailLabel">date : </span> {formatDate(date)}
+					</p>
+					<p className="examDetail">
+						<span className="examDetailLabel">Time : </span> {DateToTime(date)}
+					</p>
+					<p className="examDetail">
+						<span className="examDetailLabel">duration : </span> {duration}{" "}
+						hours
+					</p>
+					<p className="examDetail">
+						<span className="examDetailLabel">Exam Type : </span>
+						{examType}
+					</p>
+				</Box>
+			</Modal>
+		</>
+	);
 };
 
 export default ExamCard;
