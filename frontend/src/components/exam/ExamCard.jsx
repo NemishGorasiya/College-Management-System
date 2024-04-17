201aba604f3ee7758d44e1cfae09244372407267
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-04-17 11:47:49 +0530

    Completed exam page

--- frontend/src/components/exam/ExamCard.jsx
+++ frontend/src/components/exam/ExamCard.jsx
@@ -1,39 +1,84 @@
 import "./ExamCard.scss";
 import { modalStyle } from "../modal/modalStyle.js";
-import { Box, Modal } from "@mui/material";
+import { Box, IconButton, Modal } from "@mui/material";
 import { useState } from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import { DateToTime, formatDate } from "../../utils/utilityFunctions.js";
 
 const ExamCard = ({ examDetails }) => {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const {
-    name,
-    examType,
-    duration,
-    results,
-    subject,
-    semester,
-    totalMarks,
-    date,
-  } = examDetails;
-  const { name: subjectName, credits } = subject;
+  const { name, examType, results, duration, subject, totalMarks, date } =
+    examDetails;
+  const { name: subjectName, credits, subjectCode, semester } = subject;
 
   const handleCardClick = () => {
     handleOpen();
   };
   return (
-    <div className="examCardWrapper" onClick={handleCardClick}>
-      <div className="examDetailsWrapper">
-        <p className="examTitle">{name}</p>
-        <p className="examDate">{subjectName}</p>
-        <p>Know More</p>
+    <>
+      <div className="examCardWrapper" onClick={handleCardClick}>
+        <div className="examDetailsWrapper">
+          <p className="examName">{name}</p>
+          <p className="subjectName">{subjectName}</p>
+          <p>Know More</p>
+        </div>
       </div>
-      <Modal open={open} onClose={handleClose} className="uploadCircularModal">
-        <Box sx={modalStyle}></Box>
+      <Modal open={open} onClose={handleClose} className="examDetailsModal">
+        <Box sx={modalStyle}>
+          <IconButton
+            onClick={handleClose}
+            sx={{
+              position: "absolute",
+              right: 5,
+              top: 5,
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+          <p className="examTitle">{name}</p>
+          <p className="examDetail">
+            <span className="examDetailLabel">Subject : </span>
+            {subjectName}
+          </p>
+          <p className="examDetail">
+            <span className="examDetailLabel">Subject Code : </span>(
+            {subjectCode})
+          </p>
+          <p className="examDetail">
+            <span className="examDetailLabel">Semester : </span> {semester}
+          </p>
+          <p className="examDetail">
+            <span className="examDetailLabel">Total Marks : </span>
+            {totalMarks}
+          </p>
+          <p className="examDetail">
+            <span className="examDetailLabel">date : </span> {formatDate(date)}
+          </p>
+          <p className="examDetail">
+            <span className="examDetailLabel">Time : </span> {DateToTime(date)}
+          </p>
+          <p className="examDetail">
+            <span className="examDetailLabel">duration : </span> {duration}{" "}
+            hours
+          </p>
+          <p className="examDetail">
+            <span className="examDetailLabel">Subject Credits : </span>
+            {credits}
+          </p>
+          <p className="examDetail">
+            <span className="examDetailLabel">Exam Type : </span>
+            {examType}
+          </p>
+          <p className="examDetail">
+            <span className="examDetailLabel">Result : </span>
+            <span className="examResult">{results[0]?.marks ?? "Pending"}</span>
+          </p>
+        </Box>
       </Modal>
-    </div>
+    </>
   );
 };
 
--- frontend/src/utils/utilityFunctions.js
+++ frontend/src/utils/utilityFunctions.js
@@ -1,9 +1,17 @@
 export const formatDate = (date) => {
-	const dateToFormat = new Date(date);
-	const formattedDate = new Intl.DateTimeFormat("en-US", {
-		day: "2-digit",
-		month: "short",
-		year: "numeric",
-	}).format(dateToFormat);
-	return formattedDate;
+  const dateToFormat = new Date(date);
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  }).format(dateToFormat);
+  return formattedDate;
+};
+export const DateToTime = (date) => {
+  const dateToFormat = new Date(date);
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  }).format(dateToFormat);
+  return formattedDate;
 };
