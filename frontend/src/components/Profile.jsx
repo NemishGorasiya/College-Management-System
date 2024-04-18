84350cf16ab95942c4d195f6c692cfe2b4f2ccc6
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-04-18 23:36:31 +0530

    Destrucuring object in ProfilePage

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -8,84 +8,103 @@ import { AuthContext } from "../context/AuthContext";
 import { fetchProfileData } from "../services/services";
 
 export default function Profile() {
-  const [personalInfo, setPersonalInfo] = useState({
-    list: [],
-    isLoading: true,
-  });
-  const [academicInfo, setAcademicInfo] = useState({
-    list: [],
-    isLoading: true,
-  });
-  const [profilePicture, setProfilePicture] = useState(null);
-  const getProfileData = async () => {
-    try {
-      const res = await fetchProfileData();
-      const { user } = res;
+	const [personalInfo, setPersonalInfo] = useState({
+		list: [],
+		isLoading: true,
+	});
+	const [academicInfo, setAcademicInfo] = useState({
+		list: [],
+		isLoading: true,
+	});
+	const [profilePicture, setProfilePicture] = useState(null);
+	const getProfileData = async () => {
+		try {
+			const res = await fetchProfileData();
+			const { user } = res;
+			const {
+				fullName,
+				dob,
+				email,
+				gender,
+				bloodGroup,
+				phoneNumber,
+				fatherName,
+				motherName,
+				parentPhoneNumber,
+				address,
+				enrollmentNumber,
+				doa,
+				semester,
+				passOutYear,
+				department,
+				designation,
+				profilePicture,
+			} = user;
 
-      const personalInfo = [
-        { label: "Full Name", value: user.fullName },
-        { label: "Date of Birth", value: formatDate(user.dob) },
-        { label: "Email", value: user.email },
-        { label: "Gender", value: user.gender },
-        { label: "Blood Group", value: user.bloodGroup },
-        { label: "Phone Number", value: user.phoneNumber },
-        { label: "Father's Name", value: user.fatherName },
-        { label: "Mother's Name", value: user.motherName },
-        { label: "Parent's Phone Number", value: user.parentPhoneNumber },
-        { label: "Address", value: user.address },
-      ];
-      const academicInfo = [
-        { label: "Enrollment Number", value: user.enrollmentNumber },
-        { label: "Date of Admission", value: formatDate(user.doa) },
-        { label: "Semester", value: user.semester },
-        { label: "Pass Out Year", value: user.passOutYear },
-        // { label: "Department", value: user.department },
-        { label: "Designation", value: user.designation },
-      ];
+			const personalInfo = [
+				{ label: "Full Name", value: fullName },
+				{ label: "Date of Birth", value: formatDate(dob) },
+				{ label: "Email", value: email },
+				{ label: "Gender", value: gender },
+				{ label: "Blood Group", value: bloodGroup },
+				{ label: "Phone Number", value: phoneNumber },
+				{ label: "Father's Name", value: fatherName },
+				{ label: "Mother's Name", value: motherName },
+				{ label: "Parent's Phone Number", value: parentPhoneNumber },
+				{ label: "Address", value: address },
+			];
+			const academicInfo = [
+				{ label: "Enrollment Number", value: enrollmentNumber },
+				{ label: "Date of Admission", value: formatDate(doa) },
+				{ label: "Semester", value: semester },
+				{ label: "Pass Out Year", value: passOutYear },
+				// { label: "Department", value: department },
+				{ label: "Designation", value: designation },
+			];
 
-      if (user.profilePicture) {
-        setProfilePicture(user.profilePicture);
-      }
+			if (profilePicture) {
+				setProfilePicture(profilePicture);
+			}
 
-      setPersonalInfo({
-        list: personalInfo,
-        isLoading: false,
-      });
-      setAcademicInfo({
-        list: academicInfo,
-        isLoading: false,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
+			setPersonalInfo({
+				list: personalInfo,
+				isLoading: false,
+			});
+			setAcademicInfo({
+				list: academicInfo,
+				isLoading: false,
+			});
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  useEffect(() => {
-    getProfileData();
-  }, []);
+	useEffect(() => {
+		getProfileData();
+	}, []);
 
-  return (
-    <div className="profileContainer">
-      <ServiceTitle serviceTitle="Personal details" />
-      <div className="profileContentWrapper">
-        <img
-          className="profileImage"
-          src={profilePicture ?? profileImage}
-          alt=""
-        />
-        <div className="profileContent">
-          <ProfileSection
-            serviceSubTitle="Personal Information"
-            profileDetails={personalInfo.list}
-            isLoading={personalInfo.isLoading}
-          />
-          <ProfileSection
-            serviceSubTitle="Academic Information"
-            profileDetails={academicInfo.list}
-            isLoading={personalInfo.isLoading}
-          />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="profileContainer">
+			<ServiceTitle serviceTitle="Personal details" />
+			<div className="profileContentWrapper">
+				<img
+					className="profileImage"
+					src={profilePicture ?? profileImage}
+					alt=""
+				/>
+				<div className="profileContent">
+					<ProfileSection
+						serviceSubTitle="Personal Information"
+						profileDetails={personalInfo.list}
+						isLoading={personalInfo.isLoading}
+					/>
+					<ProfileSection
+						serviceSubTitle="Academic Information"
+						profileDetails={academicInfo.list}
+						isLoading={personalInfo.isLoading}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
