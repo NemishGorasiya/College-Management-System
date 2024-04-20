cf6d39d34295127c6b82a58b8844dfd4eaebaa92
NemishGorasiya <nemish.gorasiya@zuru.com>
2024-04-21 02:47:42 +0530

    Added Edit Request modal

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -7,10 +7,26 @@ import { useCallback, useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { fetchProfileData } from "../services/services";
 import fallbackProfileImageMale from "../assets/fallbackProfileImageMale.png";
+import CloseIcon from "@mui/icons-material/Close";
 import fallbackProfileImageFemale from "../assets/fallbackProfileImageFemale.png";
+import {
+	Box,
+	Button,
+	FormControl,
+	IconButton,
+	InputLabel,
+	Modal,
+	OutlinedInput,
+	Stack,
+} from "@mui/material";
+import EditNoteIcon from "@mui/icons-material/EditNote";
+import { modalStyle } from "./modal/modalStyle";
 
 export default function Profile() {
 	const { userType } = useContext(AuthContext);
+	const [open, setOpen] = useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
 	const [personalInfo, setPersonalInfo] = useState({
 		list: [],
 		isLoading: true,
@@ -19,6 +35,11 @@ export default function Profile() {
 		list: [],
 		isLoading: true,
 	});
+
+	const handleEditFormSubmit = (event) => {
+		event.preventDefault();
+		console.log("form submitted");
+	};
 	const [profilePicture, setProfilePicture] = useState(null);
 	const getProfileData = useCallback(async () => {
 		try {
@@ -47,26 +68,34 @@ export default function Profile() {
 			} = user;
 
 			let personalInfo = [
-				{ label: "Full Name", value: fullName },
-				{ label: "Date of Birth", value: formatDate(dob) },
-				{ label: "Email", value: email },
-				{ label: "Gender", value: gender },
-				{ label: "Blood Group", value: bloodGroup },
-				{ label: "Phone Number", value: phoneNumber },
-				{ label: "Father's Name", value: fatherName },
-				{ label: "Mother's Name", value: motherName },
-				{ label: "Parent's Phone Number", value: parentPhoneNumber },
-				{ label: "Address", value: address },
+				{ label: "Full Name", value: fullName, name: "fullName" },
+				{ label: "Date of Birth", value: formatDate(dob), name: "dob" },
+				{ label: "Email", value: email, name: "email" },
+				{ label: "Gender", value: gender, name: "gender" },
+				{ label: "Blood Group", value: bloodGroup, name: "bloodGroup" },
+				{ label: "Phone Number", value: phoneNumber, name: "phoneNumber" },
+				{ label: "Father's Name", value: fatherName, name: "fatherName" },
+				{ label: "Mother's Name", value: motherName, name: "motherName" },
+				{
+					label: "Parent's Phone Number",
+					value: parentPhoneNumber,
+					name: "parentPhoneNumber",
+				},
+				{ label: "Address", value: address, name: "address" },
 			];
 
 			const academicInfo = [
-				{ label: "Enrollment Number", value: enrollmentNumber },
-				// { label: "Date of Admission", value: formatDate(doa) },
-				{ label: "Date of Joining", value: formatDate(doj) },
-				{ label: "Semester", value: semester },
-				{ label: "Pass Out Year", value: passOutYear },
-				{ label: "Department", value: department.name },
-				{ label: "Designation", value: designation },
+				{
+					label: "Enrollment Number",
+					value: enrollmentNumber,
+					name: "enrollmentNumber",
+				},
+				// { label: "Date of Admission", value: formatDate(doa) ,name : "doa"},
+				{ label: "Date of Joining", value: formatDate(doj), name: "doj" },
+				{ label: "Semester", value: semester, name: "semester" },
+				{ label: "Pass Out Year", value: passOutYear, name: "passOutYear" },
+				{ label: "Department", value: department.name, name: "departmentName" },
+				{ label: "Designation", value: designation, name: "designation" },
 			];
 
 			if (userType === "admin") {
@@ -98,32 +127,98 @@ export default function Profile() {
 	}, [getProfileData]);
 
 	return (
-		<div className="profileContainer">
-			<ServiceTitle serviceTitle="Profile details" />
-			<div className="profileContentWrapper">
-				<img
-					className="profileImage"
-					src={profilePicture}
-					alt=""
-					onError={(event) => {
-						handleFallBackImage(event, fallbackProfileImageMale);
-					}}
-				/>
-				<div className="profileContent">
-					<ProfileSection
-						serviceSubTitle="Personal Information"
-						profileDetails={personalInfo.list}
-						isLoading={personalInfo.isLoading}
-					/>
-					{(userType === "student" || userType === "faculty") && (
-						<ProfileSection
-							serviceSubTitle="Academic Information"
-							profileDetails={academicInfo.list}
-							isLoading={academicInfo.isLoading}
+		<>
+			<div className="profileContainer">
+				<ServiceTitle serviceTitle="Profile details" />
+				<div className="profileContentWrapper">
+					<div className="profileImageContainer">
+						<img
+							className="profileImage"
+							src={profilePicture}
+							alt=""
+							onError={(event) => {
+								handleFallBackImage(event, fallbackProfileImageMale);
+							}}
 						/>
-					)}
+						{(userType === "student" || userType === "faculty") && (
+							<Button
+								onClick={handleOpen}
+								className="editBtn"
+								variant="contained"
+							>
+								<EditNoteIcon />
+								Edit Profile
+							</Button>
+						)}
+					</div>
+					<div className="profileContent">
+						<ProfileSection
+							serviceSubTitle="Personal Information"
+							profileDetails={personalInfo.list}
+							isLoading={personalInfo.isLoading}
+						/>
+						{(userType === "student" || userType === "faculty") && (
+							<ProfileSection
+								serviceSubTitle="Academic Information"
+								profileDetails={academicInfo.list}
+								isLoading={academicInfo.isLoading}
+							/>
+						)}
+					</div>
 				</div>
 			</div>
-		</div>
+			<Modal open={open} className="editProfileModal">
+				<Box className="dialogBox" sx={modalStyle}>
+					<IconButton
+						onClick={handleClose}
+						sx={{
+							position: "absolute",
+							right: 5,
+							top: 5,
+						}}
+					>
+						<CloseIcon />
+					</IconButton>
+					<form
+						action=""
+						className="editProfileForm"
+						onSubmit={handleEditFormSubmit}
+					>
+						{personalInfo.list.map(
+							(profileDetail) =>
+								profileDetail.value && (
+									<FormControl
+										key={profileDetail.name}
+										variant="outlined"
+										className="formControl"
+									>
+										<InputLabel htmlFor={profileDetail.name}>
+											{profileDetail.label}
+										</InputLabel>
+										<OutlinedInput
+											defaultValue={profileDetail.value}
+											id={profileDetail.name}
+											name={profileDetail.name}
+											label={profileDetail.label}
+										/>
+									</FormControl>
+								)
+						)}
+						<FormControl variant="outlined" className="formControl">
+							<InputLabel>sdoksd</InputLabel>
+							<OutlinedInput />
+						</FormControl>
+
+						<Button
+							type="submit"
+							variant="contained"
+							className="editProfileSubmitBtn"
+						>
+							Submit Edit Request
+						</Button>
+					</form>
+				</Box>
+			</Modal>
+		</>
 	);
 }
--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,5 +1,4 @@
 import "./TopBar.scss";
-import Button from "../UI/Button";
 import ldceLogo from "../assets/ldce_logo.png";
 import notificationBell from "../assets/notificationBell.png";
 import BurgerMenuIcon from "../assets/BurgerMenuIcon.svg";
@@ -9,45 +8,45 @@ import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import useLocalStorage from "../hooks/useLocalStorage";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { Button } from "@mui/material";
 
 export default function TopBar({ handleHamBurgerClick, isAuthenticated }) {
-  const navigate = useNavigate();
-  const { updateUserType } = useContext(AuthContext);
-  // const [userInfo, setUserInfo] = useLocalStorage("userInfo", null);
-  const handleLogout = async () => {
-    try {
-      const res = await logoutUser();
-      if (res.status === 200) {
-        toast.success("User loggedOut successfully");
-        updateUserType("");
-        navigate("/login/student");
-      }
-    } catch (error) {
-      toast.error("Something went wrong");
-    }
-  };
-  return (
-    <div className="topBar">
-      <div className="topBarLeft">
-        <img
-          className="burgerMenuIcon"
-          src={BurgerMenuIcon}
-          onClick={handleHamBurgerClick}
-          alt="BurgerIcon"
-        />
-        <img className="logo" src={ldceLogo} alt="ldceLogo" />
-        <span className="collegeName">L.D. College of Engineering</span>
-      </div>
-      {isAuthenticated && (
-        <div className="topBarRight">
-          <Button className="notificationBell">
-            <img src={notificationBell} alt="" />
-          </Button>
-          <Button onClick={handleLogout} textonly>
-            Log Out
-          </Button>
-        </div>
-      )}
-    </div>
-  );
+	const navigate = useNavigate();
+	const { updateUserType } = useContext(AuthContext);
+	// const [userInfo, setUserInfo] = useLocalStorage("userInfo", null);
+	const handleLogout = async () => {
+		try {
+			const res = await logoutUser();
+			if (res.status === 200) {
+				toast.success("User loggedOut successfully");
+				updateUserType("");
+				navigate("/login/student");
+			}
+		} catch (error) {
+			toast.error("Something went wrong");
+		}
+	};
+	return (
+		<div className="topBar">
+			<div className="topBarLeft">
+				<img
+					className="burgerMenuIcon"
+					src={BurgerMenuIcon}
+					onClick={handleHamBurgerClick}
+					alt="BurgerIcon"
+				/>
+				<img className="logo" src={ldceLogo} alt="ldceLogo" />
+				<span className="collegeName">L.D. College of Engineering</span>
+			</div>
+			{isAuthenticated && (
+				<div className="topBarRight">
+					<Button className="logoutBtn" onClick={handleLogout}>
+						<span className="logoutText">Logout</span>
+						<LogoutIcon />
+					</Button>
+				</div>
+			)}
+		</div>
+	);
 }
